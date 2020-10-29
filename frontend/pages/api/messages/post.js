34ad55144e49f2738992748a030f7fd3ad138d39
Edmond Marfo <edmarfo2@gmail.com>
2020-10-29 02:35:12 +0000

    set the env variables for post api

--- frontend/pages/api/messages/post.js
+++ frontend/pages/api/messages/post.js
@@ -30,9 +30,9 @@ const createMessage = async (user, message, id, check = false) => {
 
 const pusher = new Pusher({
     appId: process.env.PUSHER_ID,
-    key: process.env.PUSHER_KEY,
+    key: process.env.NEXT_PUBLIC_PUSHER_KEY,
     secret: process.env.PUSHER_SECRET,
-    cluster: process.env.PUSHER_CLUSTER,
+    cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER,
     useTLS: true
 });
 
