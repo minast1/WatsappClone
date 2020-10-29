adf9e7512b620f6605f10516bacbfa27ef394416
Edmond Marfo <edmarfo2@gmail.com>
2020-10-29 01:33:25 +0000

    set the next public env settings

--- frontend/components/Chatarea.js
+++ frontend/components/Chatarea.js
@@ -100,8 +100,8 @@ function Chatarea({ user, chatData }) {
     /*
    useEffect(() => {
 
-       const pusher = new Pusher(process.env.PUSHER_KEY, {
-           cluster: process.env.PUSHER_CLUSTER
+       const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY, {
+           cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER
        });
 
        const channel = pusher.subscribe('messages');
